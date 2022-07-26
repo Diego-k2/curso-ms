9798a529004328f664808c360697ce51228dff97
Diego-k2 <degosantosiva@gmail.com>
2022-07-26 14:48:12 -0300

    cria hr-worker

---
+++ hr-workers/src/test/java/com/diegok2/hrworkers/HrWorkersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegok2.hrworkers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
