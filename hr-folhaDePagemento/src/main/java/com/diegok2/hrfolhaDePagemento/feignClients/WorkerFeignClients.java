6ad00a3d7338ac92fce2b6cc36447735e5c3b585
Diego-k2 <degosantosiva@gmail.com>
2022-07-27 19:10:59 -0300

    implementa feign

--- hr-folhaDePagemento/src/main/java/com/diegok2/hrfolhaDePagemento/feignClients/WorkerFeignClients.java
+++ hr-folhaDePagemento/src/main/java/com/diegok2/hrfolhaDePagemento/feignClients/WorkerFeignClients.java
@@ -1,2 +1,20 @@
-package com.diegok2.hrfolhaDePagemento.feignClients;public interface WorkerFeignClients {
+package com.diegok2.hrfolhaDePagemento.feignClients;
+
+import com.diegok2.hrfolhaDePagemento.model.entity.Worker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(name = "hr-worker", url = "http://localhost:8001", path = "/workers")
+public interface WorkerFeignClients {
+
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Worker> findById(@PathVariable Long id);
+
+
+
 }
--- hr-folhaDePagemento/src/main/java/com/diegok2/hrfolhaDePagemento/service/PaymentService.java
+++ hr-folhaDePagemento/src/main/java/com/diegok2/hrfolhaDePagemento/service/PaymentService.java
@@ -1,31 +1,21 @@
 package com.diegok2.hrfolhaDePagemento.service;
 
+import com.diegok2.hrfolhaDePagemento.feignClients.WorkerFeignClients;
 import com.diegok2.hrfolhaDePagemento.model.entity.Payment;
 import com.diegok2.hrfolhaDePagemento.model.entity.Worker;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}") //para pegar algo no properties
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClients workerFeignClients;
 
     public Payment getPayment(Long workId, int diasTrabalahdos){
 
-        //Pegando paramentros da url
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", ""+ workId); //Para usar basta fazer igual na linha 28
-
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClients.findById(workId).getBody();
 
         return new Payment(worker.getNome(), worker.getValorDiario(), diasTrabalahdos);
     }
