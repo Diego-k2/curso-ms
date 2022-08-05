31cbc79b0332d7ab688fb1d0fa1cb5dcf1a146a2
Diego-k2 <degosantosiva@gmail.com>
2022-08-05 14:00:46 -0300

    refactor hr-payrool

---
+++ hr-payrool/src/main/java/br/com/diego/hrpayrool/model/entity/Worker.java
@@ -0,0 +1,37 @@
+package br.com.diego.hrpayrool.model.entity;
+
+public class Worker {
+
+    private Long id;
+
+    private String nome;
+
+    private Double valorDiario;
+
+    public Worker(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getValorDiario() {
+        return valorDiario;
+    }
+
+    public void setValorDiario(Double valorDiario) {
+        this.valorDiario = valorDiario;
+    }
+
+}
--- hr-payrool/src/main/java/br/com/diego/hrpayrool/model/services/PaymentService.java
+++ hr-payrool/src/main/java/br/com/diego/hrpayrool/model/services/PaymentService.java
@@ -1,17 +1,29 @@
 package br.com.diego.hrpayrool.model.services;
 
 
+import br.com.diego.hrpayrool.FeignClients.WorkerFeignClients;
 import br.com.diego.hrpayrool.model.entity.Payment;
+import br.com.diego.hrpayrool.model.entity.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Autowired
+    WorkerFeignClients workerFeignClients;
+
     public Payment returnPayment(Long workerId, int days){
+        Map<String, String> uriVariable = new HashMap<>();
 
+        uriVariable.put("id", String.valueOf(workerId));
 
+        Worker worker = workerFeignClients.findById(workerId).getBody();
 
-        return new Payment("Diego", 150.0, days);
+        return new Payment(worker.getNome(), worker.getValorDiario(), days);
 
     }
 }
