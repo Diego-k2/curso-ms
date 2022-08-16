310d844c32dce68870179269f72761a86e54eeed
Diego-k2 <degosantosiva@gmail.com>
2022-08-16 11:08:56 -0300

    add ramdon port to hr-worker

--- hr-payrool/src/main/java/br/com/diego/hrpayrool/HrPayroolApplication.java
+++ hr-payrool/src/main/java/br/com/diego/hrpayrool/HrPayroolApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
-@EnableEurekaClient
+@EnableFeignClients //FAZ A COMUNICAÇÃO COM O HR-WORKER
+@EnableEurekaClient //MARCA COMO CLIENTE DO EUREKA
 @SpringBootApplication
 public class HrPayroolApplication {
 
